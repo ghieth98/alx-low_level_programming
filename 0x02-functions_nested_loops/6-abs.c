a8de04eead7f3353d888ef3b6e9ec71db5d6a32f
ghieth98 <hossamghieth@gmail.com>
2023-03-21 11:59:48 -0700

    abs task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - compute absloute value of integer
+ * @i: integer value
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+
+	return (0);
+}
