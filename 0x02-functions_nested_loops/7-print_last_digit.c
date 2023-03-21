db73cc3ce6242f897a7b6979a06a83573def22df
ghieth98 <hossamghieth@gmail.com>
2023-03-21 12:37:57 -0700

    print last digit task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
 
 int print_last_digit(int i)
 {
-	if (i < 0)
-		i *= -1;
-
-	_putchar('0' + (i % 10));
-
-	return (i % 10);
+i %= 10;
+if (i < 0)
+i *= -1;
+i += 48;
+_putchar(i);
+return (i - '0');
 }
