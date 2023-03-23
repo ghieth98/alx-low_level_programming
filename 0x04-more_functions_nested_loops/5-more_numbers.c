e07b7a6fd052827c80d2964bdb231f1d5c90acad
ghieth98 <hossamghieth@gmail.com>
2023-03-23 06:31:12 -0700

    morenumbers task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 	char i;
 	int counter;
 
-	for (counter = 1; counter <= 10; conter++)
+	for (counter = 1; counter <= 10; counter++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i /10 > 0)
+			if (i / 10 > 0)
 				_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 		}
