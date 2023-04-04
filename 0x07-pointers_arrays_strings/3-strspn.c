e57b73e349f9cc940b066e6a48be2e7d51f5eb7f
ghieth98 <hossamghieth@gmail.com>
2023-04-03 19:43:24 -0700

    task 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - returns length of string that matches values consistently
+ * @s: string to search
+ * @accepy: target matches
+ * Return: number of bytes consecively matched
+ */
+
+unsigned int _strspen(char *s, char *accept)
+{
+	int i = 0, j;
+	int matches = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				matches++;
+				break;
+			}
+			if (accept[j + i] == '\0' && s[i] != accept[j])
+				return (matches);
+		}
+		i++;
+	}
+	return (matches);
+}
