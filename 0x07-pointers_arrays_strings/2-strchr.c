25949c962acb5bbff301f759988e60e0e845816a
ghieth98 <hossamghieth@gmail.com>
2023-04-03 19:33:17 -0700

    strchr task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locate 1st occurrence of char in string and returns pointer
+ * @s: string to search
+ * @c: target charcater
+ * Return: pointer to that charcater in string
+ */
+
+char *_strchr(char *s, char c)
+{
+	itn i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
+}
