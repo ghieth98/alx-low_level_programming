faa3889a0040c6661f18ee1917db4b72e2f7cd99
ghieth98 <hossamghieth@gmail.com>
2023-04-17 16:44:07 -0700

    poppy task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
