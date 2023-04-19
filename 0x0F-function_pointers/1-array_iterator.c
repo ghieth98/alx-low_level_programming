1d985a6a58cb7f551892c280bb7d9cba7b35beca
ghieth98 <hossamghieth@gmail.com>
2023-04-19 09:11:17 -0700

    array iterator task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "funciton_pointers"
+
+/**
+ * array_iterator - execute function on array via function ptr
+ * @array: array
+ * @size: size of array
+ * @action: pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+		for (i = 0; i < size, i++)
+			action(*(array + i));
+}
