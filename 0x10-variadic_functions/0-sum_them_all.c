5477a771802f09936a37423ff0f59747594fb744
ghieth98 <hossamghieth@gmail.com>
2023-04-20 13:09:36 -0700

    fix sum them all task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - sum of all its parameters
  * @n: number of parameters
- * Return: 0 if n = 0 or sum of parameters if successful
+ * Return: 0 if n = 0 or sum of parameters if succesful
  */
 
 int sum_them_all(const unsigned int n, ...)
