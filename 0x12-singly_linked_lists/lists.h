b65c8da2170acabf38918f62f61fe68f6ad876e5
ghieth98 <hossamghieth@gmail.com>
2023-04-27 08:59:50 -0700

    fixed lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,6 +20,7 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 
 #endif
