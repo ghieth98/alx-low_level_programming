49f119e4eea189eaf311d206d07a9df2a719a287
ghieth98 <hossamghieth@gmail.com>
2023-05-04 06:53:06 -0700

    fix first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int uint, place;
+	int len;
 
-	int len = 0, i;
-	unsigned int sum = 0;
+	if (!b)
+		return (0);
 
-	if (b == NULL)
-		return (sum);
-
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	i = 0;
-	while (b[i])
+	for (len = 0; b[len]; len++)
+		;
+	len--;
+	for (place = 1, uint = 0; len >= 0; len--)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[len] == '0')
+		{
+			place *= 2;
+			continue;
+		}
+		else if (b[len] == '1')
+		{
+			uint += place;
+			place *= 2;
+			continue;
+		}
+		return (0);
 	}
-
-	return (sum);
+	return (uint);
 }
